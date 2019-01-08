055b543e9ea8cb0ca8e5e27432d5f5eb9c5bf996
Zhu Wei <656686763@qq.com>
2019-01-08 14:36:59 +0800

    -界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,54 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 Vue.use(Router);
+import Home from "../components/common/Home.vue";
+import Dashboard from "../components/page/Dashboard.vue";
+import Statistics from "../components/page/statistics.vue";
+import Store from "../components/page/store.vue";
+import Supplier from "../components/page/supplier.vue";
+import Users from "../components/page/users.vue";
+export default new Router({
+    routes: [
+        
+        {
+            path: '/',
+            component: Home,
+            children:[
+                {
+                    path: '/dashboard',
+                    component:Dashboard,
+                    meta: { title: '系统首页' }
+                },
+                {
+                    path: '/store',
+                    component:Store ,
+                    meta: { title: '门店管理' }
+                },
+                {
+                    path: '/supplier',
+                    component: Supplier,
+                    meta: { title: '供应商管理' }
+                },
+                {
+                    path: '/users',
+                    component: Users,
+                    meta: { title: '用户管理' }
+                },
+                {
+                    path: '/statistics',
+                    component:Statistics,
+                    meta: { title: '统计' }
+                },
+              
+            ]
+        },
+        {
+            path: '/login',
+            component: resolve => require(['../components/page/Login.vue'], resolve)
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
+})

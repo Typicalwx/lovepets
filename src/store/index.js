4bb1e49c01ad2c1898a89e43b163f2a67bbd2c2a
mhw <962239760@qq.com>
2019-01-09 10:36:43 +0800

    1.9hebing3

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from "vue"
 import Vuex from "vuex";
 // import axios from "axios"
-import studentsModule from "./modules/studentsModule"
+import studentsModule from "./modules/studentsModule";
+import platfromModule from "./modules/platformModule"
 Vue.use(Vuex) //注册
 
 export default new Vuex.Store({
     strict: true,//严格模式
     modules: {
-        studentsModule: studentsModule //模块化的student
+        studentsModule: studentsModule, //模块化的student
+        platfromModule:platfromModule
     }
 })

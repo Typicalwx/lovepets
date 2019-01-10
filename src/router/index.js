96e236220e55c4b054655fb2ae9e4b2abbed6d67
李雷 <791412334@qq.com>
2019-01-10 11:21:07 +0800

    更改router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 Vue.use(Router);
-// import Storehome from "../components/common/Storehome.vue";
-// import Statistics from "../components/page/stores/statistics.vue";
-// import Store from "../components/page/stores/store.vue";
-// import Supplier from "../components/page/stores/supplier.vue";
-// import Users from "../components/page/stores/users.vue";
-
-// import StoreGoods from "../components/page/stores/goods/index.vue"
-// import Login from "../components/page/stores/Login.vue"
 import SupplierHome from "../components/common/supplierHome.vue";
 import Dashboard from "../components/page/suppliergoods/Dashboard.vue";
 import Statistics from "../components/page/suppliergoods/statistics.vue";
@@ -16,8 +8,8 @@ import Statistics from "../components/page/suppliergoods/statistics.vue";
 import Suppliergoods from "../components/page/suppliergoods/supplier.vue";
 import Supplier from "../components/page/supplier/sup";
 export default new Router({
-    mode: 'history',
     routes: [
+
         {
             path: '/suppliergoods',
             component: SupplierHome,
@@ -45,5 +37,6 @@ export default new Router({
 
             ]
         },
+
     ]
 })

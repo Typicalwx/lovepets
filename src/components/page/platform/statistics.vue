07e4ad4cb123c33047b8ff4272f4aea1c8a27cdd
mhw <962239760@qq.com>
2019-01-10 18:29:43 +0800

    1.10wan

--- src/components/page/platform/statistics.vue
+++ src/components/page/platform/statistics.vue
@@ -1,13 +1,35 @@
 <template>
-    <h1>统计</h1>
+  <div>
+    <el-radio-group v-model="type" @change="showChart">
+      <el-radio-button label="班级人数统计"></el-radio-button>
+      <el-radio-button label="年龄分布统计"></el-radio-button>
+      <el-radio-button label="地图分布"></el-radio-button>
+    </el-radio-group>
+    <div class="total" id="myChart" ref="myChart"></div>
+  </div>
 </template>
 
 <script>
+import echarts from "echarts/lib/echarts";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/title";
+import "echarts/lib/component/legend";
+import "echarts/extension/bmap/bmap";
+import axios from "axios";
 export default {
-
-}
+  data() {
+    return {
+      type: "地图分布"
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.showChart();
+    });
+  },
+  methods: {}
+};
 </script>
-
+    
 <style>
-
 </style>

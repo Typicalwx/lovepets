9d1b3fd9fb8304cf16b1cd278b59e730b30ab47b
Zhu Wei <656686763@qq.com>
2019-01-12 17:29:42 +0800

    wq

--- src/store/modules/storeAdministrator.js
+++ src/store/modules/storeAdministrator.js
@@ -2,16 +2,21 @@
 // import Vuex from "vuex";
 import axios from "axios";
 export default ({
-    // strict: true,
+    strict: true,
     namespaced: true,
     state: {
+        addVisible:false,
+        addDetailVisible:false,
         storeAdministrator: [],
         pagination: {},
         search: { type: "", value: "" },
     },
     mutations: {
-        setVisible(state, Visible) {
-            state.updateVisible = Visible;
+        setAddVisible(state, Visible) {
+            state.addVisible = Visible;
+        },
+        setAddDetailVisible(state, Visible) {
+            state.addDetailVisible = Visible;
         },
         setStoreAdmin(state, storeAdministrator) {
             state.storeAdministrator = storeAdministrator;
@@ -42,7 +47,22 @@ export default ({
                     value: search.value || ""
                 }
             }).then(({ data }) => {
-                console.log(data)
+                // console.log(data.rows)
+                var res = data.rows.map(function (item) {
+                    if (item.users.state == 1) {
+                        item.users.state = "可用"
+                        return item
+                    }
+                })
+                if (res[0]) {
+                    console.log(res)
+                    context.commit("setStoreAdmin", res)
+                    context.commit("setPagination", {
+                        curpage: data.curpage,
+                        maxpage: data.maxpage,
+                        total: data.total
+                    });
+                }
                 context.commit("setStoreAdmin", data.rows)
                 context.commit("setPagination", {
                     curpage: data.curpage,

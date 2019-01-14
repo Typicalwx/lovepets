04f5c2426d165893975e159efb54805e613cdc40
李雷 <791412334@qq.com>
2019-01-14 20:59:37 +0800

    合并冲突

--- src/components/page/stores/supplier.vue
+++ src/components/page/stores/supplier.vue
@@ -15,7 +15,6 @@ export default {
           click(){
               var myDate = new Date()
               console.log(myDate.toLocaleDateString()+" "+myDate.toLocaleTimeString())
-<<<<<<< HEAD
               let arr = [
                 //   {'storeId':'5c358b2d100838196886b25c','shangpingid':'5c371132bd089a29cce23fdc',goodState:"商品",shangpingname:"怡亲",num:3,price:40,total:120},
               {'storeId':'5c358b2d100838196886b25c','shangpingid':'5c3b3bd4722b1c0acca16ae7',goodState:"服务",shangpingname:"洗护",num:3,price:40,total:120},
@@ -25,16 +24,6 @@ export default {
                 // {'storeId':'5c37fe883563f80d381a7924','shangpingid':'5c36a1e0bd089a29cce23fdb',goodState:"商品",shangpingname:"凯锐思",num:3,price:30,total:120},
                 //   {'storeId':'5c37fe883563f80d381a7924','shangpingid':'5c36a1e0bd089a29cce23fdb',goodState:"商品",shangpingname:"凯锐思",num:3,price:30,total:120},
                     // {'storeId':'5c37fe883563f80d381a7924','shangpingid':'5c36a1e0bd089a29cce23fdb',goodState:"商品",shangpingname:"凯锐思",num:3,price:30,total:120}
-=======
-              let arr = [{'storeId':'5c3994a0443b711ca8c60b6d','shangpingid':'5c3b2eb9443b711ca8c60bbe',goodState:"商品",shangpingname:"唐钰朋牌猫粮",num:3,price:50,total:150},
-              {'storeId':'5c3994a0443b711ca8c60b6d','shangpingid':'5c3b2fbb443b711ca8c60bbf',goodState:"商品",shangpingname:"唐钰朋牌狗粮",num:3,price:45,total:135},
-              {'storeId':'5c3994a0443b711ca8c60b6d','shangpingid':'5c3b3022443b711ca8c60bc0',goodState:"服务",shangpingname:"唐钰朋小猫咪洗护",num:10,price:15,total:150},
-               {'storeId':'5c3994a0443b711ca8c60b6d','shangpingid':'5c3602ba1a795020cc0af419',goodState:"服务",shangpingname:"唐钰朋小狗寄养",num:10,price:30,total:300},
-              {'storeId':'5c39b47a443b711ca8c60b75','shangpingid':'5c3b334a443b711ca8c60bc4',goodState:"商品",shangpingname:"韩家秀牌猫娘",num:3,price:40,total:120},
-                {'storeId':'5c39b47a443b711ca8c60b75','shangpingid':'5c3b33be443b711ca8c60bc5',goodState:"商品",shangpingname:"韩家秀牌狗粮",num:3,price:40,total:120},
-                  {'storeId':'5c39b47a443b711ca8c60b75','shangpingid':'5c3b3168443b711ca8c60bc2',goodState:"服务",shangpingname:"韩家修小猫洗护",num:3,price:20,total:60},
-                    {'storeId':'5c39b47a443b711ca8c60b75','shangpingid':'5c3b3183443b711ca8c60bc3',goodState:"服务",shangpingname:"韩家修小狗寄养",num:3,price:30,total:90}
->>>>>>> typ
               ]
                 axios({
                     method:"post",
